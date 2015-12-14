fb21aa7054a1fe6300851dd9f57f70f9dec2e4ab
Pat Chutipong <pat.chutipong@gmail.com>
2015-12-13 20:14:39 -0600

    contact us page modified and fixed glitch on email confirmation

--- app/src/main/java/com/cs465/groceryrun/groceryrun/ContactUs.java
+++ app/src/main/java/com/cs465/groceryrun/groceryrun/ContactUs.java
@@ -1,7 +1,9 @@
 package com.cs465.groceryrun.groceryrun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ContactUs extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+    }
+
+    public void home(View v){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
